6d4294bf92b53fb16107d45cfb0359f32380cdd1
Nelson Almeida <63735406+NelsonAlmeida-18@users.noreply.github.com>
2021-04-10 08:04:05 +0100

    Updates
    Updates, contudo a função print_stack não funciona e certas macros de conversão revelam erros

--- Código/guiao2la/parser.c
+++ Código/guiao2la/parser.c
@@ -6,61 +6,83 @@
 
 //criar nova stack para ir acomulando os elementos
 #define DOLLAR_SIGN(nums,_stack)\
+	STACK *c = create_stack();\
 	int i =0;\
-	while(i<nums)\
-
+	while(i<nums){\
+		DATA X=pop(_stack);\
+		push(c,X);\
+		i+=1;\
+	}\
+	DATA MY_VAR=pop(_stack);\
+	push(_stack,MY_VAR);\
+	while(i>0){\
+		DATA x = pop(c);\
+		push(_stack,x);\
+	}\
+	push(_stack,MY_VAR); \
+//remove ultimo item
 #define PONTO_VIRGULA(_stack)\
 	pop(_stack);
-
+//troca ordem dos dois ultimos
 #define BARRA(_stack)\
 	DATA x=pop(_stack); \
 	DATA y=pop(_stack); \
 	push(_stack,x); \
 	push(_stack,y);
-
+//subtrai
 #define MENOS(_stack)\
-	if(hastype(top(_stack),STRING)==0){ \
+	if(has_type(top(_stack),STRING)==0){ \
 		INTEGER X = pop(_stack); \
 		INTEGER Y = pop(_stack); \
 		push_STRING(_stack,X-Y);\
 	}\
-	else if(hastype(top(_stack),INTEGER)==0){ \
+	else if(has_type(top(_stack),INTEGER)==0){ \
 		INTEGER X = pop(_stack); \
 		INTEGER Y = pop(_stack); \
 		push_STRING(_stack,X-Y);\
 	}\
-	else if(hastype(top(_stack),DOUBLE)==0){ \
+	else if(has_type(top(_stack),DOUBLE)==0){ \
 		NUMBER X = pop(_stack); \
 		NUMBER Y = pop(_stack); \
 		push_STRING(_stack,X-Y);\
 	}\
-	if(hastype(top(_stack),NUMBER)==0){ \
+	if(has_type(top(_stack),NUMBER)==0){ \
 		NUMBER X = pop(_stack); \
 		NUMBER Y = pop(_stack); \
 		push_STRING(_stack,X-Y);\
 	}
-
+//soma
 #define MAIS(_stack)\
-	if(hastype(top(_stack),STRING)==0){ \
+	if(has_type(top(_stack),STRING)==0){ \
 		INTEGER X = pop(_stack); \
 		INTEGER Y = pop(_stack); \
 		push_STRING(_stack,X+Y);\
 	}\
-	else if(hastype(top(_stack),INTEGER)==0){ \
+	else if(has_type(top(_stack),INTEGER)==0){ \
 		INTEGER X = pop(_stack); \
 		INTEGER Y = pop(_stack); \
 		push_STRING(_stack,X+Y);\
 	}\
-	else if(hastype(top(_stack),DOUBLE)==0){ \
+	else if(has_type(top(_stack),DOUBLE)==0){ \
 		NUMBER X = pop(_stack); \
 		NUMBER Y = pop(_stack); \
 		push_STRING(_stack,X+Y);\
 	}\
-	if(hastype(top(_stack),NUMBER)==0){ \
+	if(has_type(top(_stack),NUMBER)==0){ \
 		NUMBER X = pop(_stack); \
 		NUMBER Y = pop(_stack); \
 		push_STRING(_stack,X+Y);\
 	}
+//duplica o ultimo item
+#define UNDERSCORE(_stack)\
+	DATA x = pop(_stack);\
+	push(_stack,x);\
+	push(_stack,x);\
+
+#define i(_stack)\
+	DATA x = pop(_stack);\
+	int test = (int)x;\
+	push_LONG(_stack,test);
 
 void parse(char *line){
 	STACK *s = create_stack();
@@ -72,8 +94,18 @@ void parse(char *line){
 		else if(strcmp(token, "\\")==0){
 			BARRA(s);
 		}
+		else if(strcmp(token, "_")==0){
+			UNDERSCORE(s);
+		}
+//para já hardcoded mas facil correção com um pop antes para determinar o numero de elementos que teão de ser viajados
+		else if(strcmp(token, "$")==0){
+			DOLLAR_SIGN(2,s);
+		}
 		else if(strcmp(token, "-")==0){
-			BARRA(s);
+			MENOS(s);
+		}
+		else if(strcmp(token, "i")==0){
+			i(s);
 		}
 	}
 	print_stack(s);
