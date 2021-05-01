e97738a96aefada89ddf0697cc371b539628fce1
Hugocrc <79588863+Hugocrc@users.noreply.github.com>
2021-05-01 13:03:37 +0100

    Add files via upload

--- testes/teste.c
+++ testes/teste.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+  *\brief Funçao teste que printa "Hello, World".
+  *@param Nao recebe parametros.
+  *@returns Esta função não tem retorno.
+  */
 void main(){
 	printf("Hello, World");
 }
