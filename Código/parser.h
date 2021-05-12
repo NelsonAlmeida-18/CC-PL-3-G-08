64fc79df003bd0068eda67b80d4a6b124786ec09
José Pedro <72660243+JosePedro-31@users.noreply.github.com>
2021-05-12 14:52:39 +0100

    Delete parser.h

--- Código/parser.h
+++
@@ -1,7 +0,0 @@
-#include "stack.h"
-/**
- *@file Neste ficheiro é inicializada a função parse que requer 0 argumentos.
- *@arg A função parse não requer qualquer argumento.
-*/ 
-void fun_getsnewline(STACK *s);
-void parse();
